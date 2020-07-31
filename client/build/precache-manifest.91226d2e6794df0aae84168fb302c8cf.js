3060831be1a5a8d2fe9b6cb954cfe136a4741263
Bisengo Kumbu <kbisengo@yahoo.fr>
2020-07-31 13:11:51 -0500

    added build

---
+++ client/build/precache-manifest.91226d2e6794df0aae84168fb302c8cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc879a986805660b565f351c25080aee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3a502235468ee309685",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58a891fd689c165da22a",
+    "url": "/static/js/2.31a616af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.31a616af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3a502235468ee309685",
+    "url": "/static/js/main.95dde0ba.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
